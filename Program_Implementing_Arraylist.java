576d2130e2748447f16519ebfd9aa4efc8bab7ca
Tanisha Srivastava <138584422+tanishasrivastava@users.noreply.github.com>
2023-08-21 21:56:35 +0530

    Create   Program_Implementing_Arraylist.java

---
+++ Program_Implementing_Arraylist.java
@@ -0,0 +1,11 @@
+import java.util.*;  
+public class TestJavaCollection1 {
+public static void main(String args[]){  
+ArrayList<Integer> list=new ArrayList<Integer>();//Creating arraylist  
+list.add(1);//Adding object in arraylist  
+list.add(2);  
+
+//Traversing list through Iterator  
+System.out.println(list);
+}  
+}  
